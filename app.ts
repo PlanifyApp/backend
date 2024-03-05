6f2a573c139743c5835d1be7052b6894ac7ddf7d
jean young Park <jean.young.park.96@gmail.com>
2024-03-05 14:32:43 +0900

    core: 타입스크립트 정의

--- app.ts
+++ app.ts
@@ -1,3 +1,5 @@
+import { ErrorRequestHandler, NextFunction, Request, Response } from "express";
+
 var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
@@ -8,10 +10,6 @@ var indexRouter = require('./routes/index');
 
 var app = express();
 
-// view engine setup
-// app.set('views', path.join(__dirname, 'views'));
-// app.set('view engine', 'jade');
-
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
@@ -21,12 +19,12 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/api', indexRouter);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function(req:Request, res:Response, next: NextFunction) {
   next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+const errorHandler: ErrorRequestHandler  = (err, req, res, next) => {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
@@ -34,6 +32,8 @@ app.use(function(err, req, res, next) {
   // render the error page
   res.status(err.status || 500);
   res.render('error');
-});
+}
+
+app.use(errorHandler);
 
 module.exports = app;
