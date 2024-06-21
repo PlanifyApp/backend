90d68cc806f486dfc7f0651ace95fb46d4533b10
JeanYoungPark <jean.young.park.96@gmail.com>
2024-06-21 14:12:53 +0900

    dev: 할 일 개발중

--- src/routes/schedule.ts
+++ src/routes/schedule.ts
@@ -4,65 +4,9 @@ import { ObjectId, Types } from "mongoose";
 
 export const scheduleRouter = express.Router();
 
-// scheduleRouter.get("/list", async (req: any, res: any) => {
-//     const user = req.user?.user;
-//     const date = req.query.date;
-
-//     if (!user) {
-//         return res.status(200).json({
-//             data: {
-//                 status: 401,
-//                 message: "로그인이 필요합니다.",
-//             },
-//         });
-//     }
-
-//     if (!date) {
-//         return res.status(200).json({
-//             data: {
-//                 status: 400,
-//                 message: "date가 필요합니다.",
-//             },
-//         });
-//     }
-
-//     const schedule = await Schedule.find({
-//         userId: user._id as ObjectId,
-//         startDate: {
-//             $gte: new Date(date + "T00:00:00Z"),
-//             $lte: new Date(date + "T23:59:59Z"),
-//         },
-//         endDate: {
-//             $gte: new Date(date + "T00:00:00Z"),
-//             $lte: new Date(date + "T23:59:59Z"),
-//         },
-//         isShow: "Y",
-//     });
-
-//     // const dataList: { id: string; title: string; date: string; isDone: string }[] = [];
-
-//     schedule.forEach((element) => {
-//         // const newDate = new Date(element.date);
-//         // const year = newDate.getFullYear();
-//         // const month = String(newDate.getMonth() + 1).padStart(2, "0"); // 월은 0부터 시작하므로 1을 더하고, 문자열로 변환합니다.
-//         // const day = String(newDate.getDate()).padStart(2, "0");
-
-//         // dataList.push({
-//         //     id: element._id.toString(),
-//         //     title: element.title,
-//         //     date: `${year}-${month}-${day}`,
-//         //     isDone: element.isDone,
-//         // });
-//     });
-
-//     return res.status(200).json({
-//         data: {
-//             status: 200,
-//             dataList,
-//         },
-//     });
-// });
-
+/**
+ *
+ */
 scheduleRouter.post("/store", async (req: any, res: any) => {
     const user = req.user?.user;
     const { title, memo, stDate, enDate, groupId } = req.body;
@@ -106,123 +50,80 @@ scheduleRouter.post("/store", async (req: any, res: any) => {
     }
 });
 
-// scheduleRouter.put("/check/:id", async (req: any, res: any) => {
-//     const user = req.user?.user;
-//     const { id } = req.params;
+scheduleRouter.get("/month", async (req: any, res: any) => {
+    const user = req.user?.user;
+    const { year, month } = req.query;
 
-//     if (!user) {
-//         return res.status(200).json({
-//             data: {
-//                 status: 401,
-//                 message: "로그인이 필요합니다.",
-//             },
-//         });
-//     }
+    if (!user) {
+        return res.status(200).json({
+            data: {
+                status: 401,
+                message: "로그인이 필요합니다.",
+            },
+        });
+    }
 
-//     if (!id) {
-//         return res.status(200).json({
-//             data: {
-//                 status: 400,
-//                 message: "id가 필요합니다.",
-//             },
-//         });
-//     }
+    if (!year || !month) {
+        return res.status(200).json({
+            data: {
+                status: 400,
+                message: "yarn, month가 필요합니다.",
+            },
+        });
+    }
 
-//     const obId = id as ObjectId;
-//     const ScheduleData = await Schedule.findOne({ userId: user._id as ObjectId, _id: obId });
+    const startDate = new Date(`${year}-${month.padStart(2, "0")}-01T00:00:00Z`);
+    const lastDay = new Date(year, month, 0).getDate(); // 해당 월의 마지막 날 계산
+    const endDate = new Date(`${year}-${month.padStart(2, "0")}-${lastDay}T23:59:59Z`);
 
-//     if (!ScheduleData) {
-//         return res.status(200).json({
-//             data: {
-//                 status: 400,
-//                 message: "존재하지 않는 id입니다.",
-//             },
-//         });
-//     }
+    try {
+        const monthlySchedule = await Schedule.aggregate([
+            {
+                $match: {
+                    userId: new Types.ObjectId(user._id),
+                    startDate: {
+                        $gte: startDate,
+                    },
+                    endDate: {
+                        $lte: endDate,
+                    },
+                    isShow: "Y",
+                },
+            },
+            {
+                $sort: { startDate: 1 }, // 날짜 순으로 정렬
+            },
+        ]);
 
-//     const newIsDone = ScheduleData.isDone === "Y" ? "N" : "Y";
-//     const updateData = await Schedule.findOneAndUpdate(
-//         { userId: user._id as ObjectId, _id: obId },
-//         { isDone: newIsDone, modifiedAt: new Date() },
-//         { new: true }
-//     );
+        const dataList: { [key: string]: { id: string; title: string; memo: string; groupId: string; startDate: string; endDate: string }[] } = {};
 
-//     if (updateData) {
-//         return res.status(200).json({
-//             data: {
-//                 status: 200,
-//                 updateData,
-//             },
-//         });
-//     }
-// });
+        monthlySchedule.forEach((element) => {
+            const startDateParse = element.startDate.toISOString().replace("T", " ").replace(/\..+/, "");
+            const startDateDay = startDateParse.split(/(\s+)/)[0].split("-")[2];
+            const endDateParse = element.endDate.toISOString().replace("T", " ").replace(/\..+/, "");
 
-// scheduleRouter.get("/month", async (req: any, res: any) => {
-//     const user = req.user?.user;
-//     const { year, month } = req.query;
+            if (!dataList[startDateDay]) {
+                dataList[startDateDay] = [];
+            }
 
-//     if (!user) {
-//         return res.status(200).json({
-//             data: {
-//                 status: 401,
-//                 message: "로그인이 필요합니다.",
-//             },
-//         });
-//     }
+            dataList[startDateDay].push({
+                id: element._id.toString(),
+                title: element.title,
+                memo: element.memo,
+                groupId: element.groupId.toString(),
+                startDate: startDateParse,
+                endDate: endDateParse,
+            });
+        });
 
-//     if (!year || !month) {
-//         return res.status(200).json({
-//             data: {
-//                 status: 400,
-//                 message: "yarn, month가 필요합니다.",
-//             },
-//         });
-//     }
-
-//     const startDate = new Date(`${year}-${month.padStart(2, "0")}-01T00:00:00Z`);
-//     const lastDay = new Date(year, month, 0).getDate(); // 해당 월의 마지막 날 계산
-//     const endDate = new Date(`${year}-${month.padStart(2, "0")}-${lastDay}T23:59:59Z`);
-
-//     try {
-//         const monthlySchedule = await Schedule.aggregate([
-//             {
-//                 $match: {
-//                     userId: new Types.ObjectId(user._id),
-//                     date: {
-//                         $gte: startDate,
-//                         $lte: endDate,
-//                     },
-//                     isShow: "Y",
-//                 },
-//             },
-//             {
-//                 $group: {
-//                     _id: { $dateToString: { format: "%Y-%m-%d", date: "$date" } },
-//                     count: { $sum: 1 },
-//                 },
-//             },
-//             {
-//                 $sort: { _id: 1 }, // 날짜 순으로 정렬
-//             },
-//         ]);
-
-//         const dataList: number[] = new Array(lastDay).fill(0);
-
-//         monthlySchedule.forEach((element) => {
-//             const dateString = element._id;
-//             const parts = dateString.split("-");
-//             const day = parts[2];
-
-//             dataList[day] = element.count;
-//         });
-
-//         return res.status(200).json({
-//             data: {
-//                 status: 200,
-//                 Schedule: dataList,
-//             },
-//         });
-//     } catch (error) {
-//         console.log(error);
-//     }
-// });
+        // console.log(dataList);
+        return res.status(200).json({
+            data: {
+                status: 200,
+                schedule: dataList,
+            },
+        });
+    } catch (error) {
+        console.log(error);
+    }
+});
