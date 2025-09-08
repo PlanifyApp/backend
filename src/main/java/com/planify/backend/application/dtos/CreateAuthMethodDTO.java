69451bad513c9f5cc4f3fc3081e4cb632c0529bf
Daniel Melendez Ramirez <daniel.melendez@correounivalle.edu.co>
2025-09-07 21:10:35 -0500

    Fix: Using lombok in AccountsEntity and AccountsCreateDTO files

--- src/main/java/com/planify/backend/application/dtos/CreateAuthMethodDTO.java
+++ src/main/java/com/planify/backend/application/dtos/CreateAuthMethodDTO.java
@@ -1,31 +1,16 @@
 package com.planify.backend.application.dtos;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class CreateAuthMethodDTO {
 
     private Integer userId;
     private String provider;
-    private String password; // solo se usa si provider = "local"
+    private String password; // nota: solo se usa si provider = "local"
     private String providerUserId;
-
-    public CreateAuthMethodDTO() {}
-
-    public CreateAuthMethodDTO(Integer userId, String provider, String password, String providerUserId) {
-        this.userId = userId;
-        this.provider = provider;
-        this.password = password;
-        this.providerUserId = providerUserId;
-    }
-
-    // Getters y setters
-    public Integer getUserId() { return userId; }
-    public void setUserId(Integer userId) { this.userId = userId; }
-
-    public String getProvider() { return provider; }
-    public void setProvider(String provider) { this.provider = provider; }
-
-    public String getPassword() { return password; }
-    public void setPassword(String password) { this.password = password; }
-
-    public String getProviderUserId() { return providerUserId; }
-    public void setProviderUserId(String providerUserId) { this.providerUserId = providerUserId; }
 }
