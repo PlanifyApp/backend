d4843fda14f6c9862adef6095e24a14021dd78cd
Juan Hoyos <juanhoyos1347@gmail.com>
2025-11-23 13:19:53 -0500

    Feat: remove wallet id and percent_spent

--- src/main/java/com/planify/backend/infrastructure/repositories/CategoriesRepository.java
+++ src/main/java/com/planify/backend/infrastructure/repositories/CategoriesRepository.java
@@ -1,6 +1,5 @@
 package com.planify.backend.infrastructure.repositories;
 
-import com.planify.backend.application.dtos.CategoryStatsResponse;
 import com.planify.backend.domain.models.CategoryEntity;
 import org.springframework.data.r2dbc.repository.Query;
 import org.springframework.data.repository.reactive.ReactiveCrudRepository;
@@ -13,24 +12,25 @@ public interface CategoriesRepository extends ReactiveCrudRepository<CategoryEnt
     UPDATE categories
     SET name = $2,
         budgeted = $3,
-        percent_spent = $4,
-        user_id = $5,
-        type = CAST($6 AS category_type_enum)
+        user_id = $4,
+        type = CAST($5 AS category_type_enum)
     WHERE id = $1
     RETURNING *
-""")
-    Mono<CategoryEntity> updateWithEnumCast(Long id, String name, Integer budgeted, Double percentSpent, Integer userId, String type);
+    """)
+    Mono<CategoryEntity> updateWithEnumCast(Long id, String name, Integer budgeted, Integer userId, String type);
+
     Flux<CategoryEntity> findByUserId(Integer userId);
+
     Mono<CategoryEntity> findByIdAndUserId(Long id, Long userId);
+
     @Query("SELECT * FROM categories WHERE user_id = :userId AND type = CAST(:type AS category_type_enum)")
     Flux<CategoryEntity> findByUserIdAndType(Integer userId, String type);
 
-
     @Query("""
-        INSERT INTO categories (name, budgeted, percent_spent, user_id, type)
-        VALUES ($1, $2, $3, $4, CAST($5 AS category_type_enum))
+        INSERT INTO categories (name, budgeted, user_id, type)
+        VALUES ($1, $2, $3, CAST($4 AS category_type_enum))
         RETURNING *
     """)
-    Mono<CategoryEntity> insertCategory(String name, Integer budgeted, Double percentSpent, Integer userId, String type);
+    Mono<CategoryEntity> insertCategory(String name, Integer budgeted, Integer userId, String type);
 
 }
